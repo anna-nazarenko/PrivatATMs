7a66bab67c016f2505a3c5d6c98ad8f957fccc00
anna-nazarenko <anna.nazarenko.dev@gmail.com>
2022-02-01 10:56:35 +0200

    Fixed the error: "Expected to decode String but found an array instead" in cityEN.

--- PrivatATMs/Networking/Models/Device/Device.swift
+++ PrivatATMs/Networking/Models/Device/Device.swift
@@ -15,13 +15,41 @@ struct Device: Codable {
     let type: String
     let cityRU: String
     let cityUA: String
-    let cityEN: String
+    let cityEN: CityEn
     let fullAddressRu: String
     let fullAddressUa: String
-    let fullAddressEn: String
+    let fullAddressEn: String?
     let placeRu: String
     let placeUa: String
     let latitude: String
     let longitude: String
     let tw: Schedule
+}
+
+enum CityEn: Codable {
+    case string(String)
+    case stringArray([String])
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let decodedValue = try? container.decode([String].self) {
+            self = .stringArray(decodedValue)
+            return
+        }
+        if let decodedValue = try? container.decode(String.self) {
+            self = .string(decodedValue)
+            return
+        }
+        throw DecodingError.typeMismatch(CityEn.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for cityEn"))
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .string(let value):
+            try container.encode(value)
+        case .stringArray(let value):
+            try container.encode(value)
+        }
+    }
 }
