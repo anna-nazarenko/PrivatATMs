8fee63cdaa20e356a55ef70c964ce5da846a1e34
anna-nazarenko <anna.nazarenko.dev@gmail.com>
2022-02-23 09:50:37 +0200

    Added String Extension

---
+++ PrivatATMs/Extensions/String+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  String+Extensions.swift
+//  PrivatATMs
+//
+//  Created by Anna Nazarenko on 18.02.2022.
+//
+
+import Foundation
+
+extension String {
+    func percentEncode() -> String? {
+        return self.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
+    }
+}
