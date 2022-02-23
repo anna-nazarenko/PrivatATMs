a54753e17f0776d866b7403f47fc128198a672a6
anna-nazarenko <anna.nazarenko.dev@gmail.com>
2022-02-23 09:54:06 +0200

    Extended UIViewController to add a SwiftUI View

--- PrivatATMs/Extensions/UIViewController+Extensions.swift
+++ PrivatATMs/Extensions/UIViewController+Extensions.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import SwiftUI
 
 fileprivate var activityIndicator: UIActivityIndicatorView?
+
+//MARK: - Activity Indicator Methods
 
 extension UIViewController {
 
@@ -18,7 +21,7 @@ extension UIViewController {
         
         activityIndicator.translatesAutoresizingMaskIntoConstraints = false
     
-        view.addSubview(activityIndicator)
+        self.view.addSubview(activityIndicator)
         
         activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
@@ -30,5 +33,29 @@ extension UIViewController {
     func removeActivityIndicator() {
         activityIndicator?.stopAnimating()
         activityIndicator?.removeFromSuperview()
+    }
+}
+
+//MARK: - Adding SwiftUI View Method
+
+extension UIViewController {
+    func addSwiftUIView<Content>(_ swiftUIView: Content, to view: UIView) where Content : View {
+        let hostingController = UIHostingController(rootView: swiftUIView)
+        
+        addChild(hostingController)
+        view.addSubview(hostingController.view)
+        
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraints = [
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.leftAnchor.constraint(equalTo: view.leftAnchor),
+            view.bottomAnchor.constraint(equalTo: hostingController.view.bottomAnchor),
+            view.rightAnchor.constraint(equalTo: hostingController.view.rightAnchor)
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+
+        hostingController.didMove(toParent: self)
     }
 }
