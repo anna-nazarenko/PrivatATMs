dd6a3519cfa1f7aab48a600d72166bcc519b4c61
anna-nazarenko <anna.nazarenko.dev@gmail.com>
2022-02-24 12:01:05 +0200

    Сhanged the indentation to two spaces

--- PrivatATMs/ViewControllers/DetailsViewController/DetailsViewController.swift
+++ PrivatATMs/ViewControllers/DetailsViewController/DetailsViewController.swift
@@ -8,44 +8,44 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-    
-    @IBOutlet weak var address: UILabel!
-    @IBOutlet weak var tableView: UITableView!
-    
-    var device: Device?
-
-    
-//MARK: - ViewController Life Cycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loadData()
-        self.tableView.dataSource = self
-        self.tableView.register(UINib(nibName: "WorkingHoursCell", bundle: nil), forCellReuseIdentifier: "WorkingHoursCell")
-    }
-    
-//MARK: - Data Manipulation Methods
-    
-    func loadData() {
-        guard let deviceInfo = self.device else { return }
-        self.address.text = deviceInfo.fullAddressUa
-    }
+  
+  @IBOutlet weak var address: UILabel!
+  @IBOutlet weak var tableView: UITableView!
+  
+  var device: Device?
+  
+  
+  //MARK: - ViewController Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    loadData()
+    self.tableView.dataSource = self
+    self.tableView.register(UINib(nibName: "WorkingHoursCell", bundle: nil), forCellReuseIdentifier: "WorkingHoursCell")
+  }
+  
+  //MARK: - Data Manipulation Methods
+  
+  func loadData() {
+    guard let deviceInfo = self.device else { return }
+    self.address.text = deviceInfo.fullAddressUa
+  }
 }
 
 //MARK: - TableView Datasource/Delegate Methods
 
 extension DetailsViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.device?.tw?.numberOfDays ?? 0
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.device?.tw?.numberOfDays ?? 0
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "WorkingHoursCell", for: indexPath) as! WorkingHoursCell
+    
+    if let workingHoursInfo = self.device?.tw?.getWorkingHoursByDay(indexPath.row) {
+      cell.configureCell(workingHoursByDay: workingHoursInfo)
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "WorkingHoursCell", for: indexPath) as! WorkingHoursCell
-
-        if let workingHoursInfo = self.device?.tw?.getWorkingHoursByDay(indexPath.row) {
-            cell.configureCell(workingHoursByDay: workingHoursInfo)
-        }
-
-        return cell
-    }
+    return cell
+  }
 }
--- PrivatATMs/ViewControllers/DevicesViewController/DevicesViewController.swift
+++ PrivatATMs/ViewControllers/DevicesViewController/DevicesViewController.swift
@@ -8,96 +8,96 @@
 import UIKit
 
 protocol DevicesViewControllerProtocol {
-    func pushDetailsVC()
-    func reloadData()
-    func showSpinner()
-    func hideSpinner()
+  func pushDetailsVC()
+  func reloadData()
+  func showSpinner()
+  func hideSpinner()
 }
 
 class DevicesViewController: UITableViewController {
-    
-    private var devicesPresenter: DevicesPresenterProtocol?
-    
-    
-//MARK: - ViewController Life Cycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.devicesPresenter = DevicesPresenter(self)
-        self.devicesPresenter?.getDevices()
-    }
+  
+  private var devicesPresenter: DevicesPresenterProtocol?
+  
+  
+  //MARK: - ViewController Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.devicesPresenter = DevicesPresenter(self)
+    self.devicesPresenter?.getDevices()
+  }
 }
 
 //MARK: - TableView Datasource/Delegate Methods
 
 extension DevicesViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.devicesPresenter?.devices?.count ?? 0
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.devicesPresenter?.devices?.count ?? 0
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ATMCell", for: indexPath)
+    if let devices = self.devicesPresenter?.devices {
+      cell.textLabel?.text = devices[indexPath.row].placeUa
     }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ATMCell", for: indexPath)
-        if let devices = self.devicesPresenter?.devices {
-            cell.textLabel?.text = devices[indexPath.row].placeUa
-        }
-        return cell
-    }
-    
-//MARK: - TableView Delegate Methods
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        pushDetailsVC()
-    }
+    return cell
+  }
+  
+  //MARK: - TableView Delegate Methods
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    pushDetailsVC()
+  }
 }
 
 //MARK: - Search Bar Delegate Methods
 
 extension DevicesViewController: UISearchBarDelegate {
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
-        if let searchBarText = searchBar.text, !searchBarText.isEmpty {
-            self.devicesPresenter?.getDevices(byCity: searchBarText)
-        }
-        searchBar.resignFirstResponder()
-    }
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        guard let searchBarText = searchBar.text else { return }
-
-        if searchBarText.count < 3 {
-            self.devicesPresenter?.updateTableViewWithPreSavedDevices()
-        } else {
-            self.devicesPresenter?.cancelRequest()
-            self.devicesPresenter?.getDevices(byCity: searchBarText)
-        }
+    if let searchBarText = searchBar.text, !searchBarText.isEmpty {
+      self.devicesPresenter?.getDevices(byCity: searchBarText)
     }
+    searchBar.resignFirstResponder()
+  }
+  
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    guard let searchBarText = searchBar.text else { return }
     
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        searchBar.endEditing(true)
+    if searchBarText.count < 3 {
+      self.devicesPresenter?.updateTableViewWithPreSavedDevices()
+    } else {
+      self.devicesPresenter?.cancelRequest()
+      self.devicesPresenter?.getDevices(byCity: searchBarText)
     }
+  }
+  
+  func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+    searchBar.endEditing(true)
+  }
 }
 
 //MARK: - Devices View Controller Protocol
 
 extension DevicesViewController: DevicesViewControllerProtocol {
-    func reloadData() {
-        self.tableView.reloadData()
+  func reloadData() {
+    self.tableView.reloadData()
+  }
+  
+  func showSpinner() {
+    self.addActivityIndicator()
+  }
+  
+  func hideSpinner() {
+    self.removeActivityIndicator()
+  }
+  
+  func pushDetailsVC() {
+    let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    let detailsVC = storyBoard.instantiateViewController(withIdentifier: "DetailsVC") as! DetailsViewController
+    if let indexPath = tableView.indexPathForSelectedRow, let devices = self.devicesPresenter?.devices {
+      detailsVC.device = devices[indexPath.row]
     }
-    
-    func showSpinner() {
-        self.addActivityIndicator()
-    }
-    
-    func hideSpinner() {
-        self.removeActivityIndicator()
-    }
-    
-    func pushDetailsVC() {
-        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let detailsVC = storyBoard.instantiateViewController(withIdentifier: "DetailsVC") as! DetailsViewController
-        if let indexPath = tableView.indexPathForSelectedRow, let devices = self.devicesPresenter?.devices {
-            detailsVC.device = devices[indexPath.row]
-        }
-        self.navigationController?.pushViewController(detailsVC, animated: true)
-    }
+    self.navigationController?.pushViewController(detailsVC, animated: true)
+  }
 }
